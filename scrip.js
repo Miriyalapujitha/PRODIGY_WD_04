446a0bc0b9c86f3bfa8164ba9f5a774620b2f0a4
Miriyalapujitha <153705019+Miriyalapujitha@users.noreply.github.com>
2024-09-23 16:04:04 +0530

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,5 @@
+// Add event listener to button
+document.querySelector('button').addEventListener('click', function() {
+    // Scroll to projects section
+    document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
+});
